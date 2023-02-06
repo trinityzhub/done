28997e32364054cdc7007a2637bbc2bc760c97b5
trinityzhub <trinity0091+github@gmail.com>
2023-02-06 16:21:12 +0000

    Try#3

--- Epsilon/E45/Ubbe/sudo/shell/docker_rel.sh
+++ Epsilon/E45/Ubbe/sudo/shell/docker_rel.sh
@@ -18,4 +18,13 @@ $ echo <USER>:10000:65536 >> /etc/subgid
 
 
 
-
+## ref : https://stackoverflow.com/questions/72281976/docker-is-running-docker-desktop-says-docker-desktop-stopped
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+sudo usermod -aG docker YOUR_USER
+sudo apt install gnome-terminal
+sudo apt install qemu-kvm libvirt-clients libvirt-daemon-system bridge-utils virtinst libvirt-daemon virt-manager -y
+sudo usermod -aG kvm YOUR_USER
+sudo apt autoremove
+sudo apt install ./docker-desktop-4.8.1-amd64.deb
+sudo systemctl disable docker.service
+sudo systemctl disable docker.socket
